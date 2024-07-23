f30d52e4c30c6fe1297b0bf70e33b4a1ab897a20
please-work-916 <mike.derrer1@gmail.com>
2024-07-23 10:16:16 -0700

    Update main.tf
    Asdded cloudfront

--- main.tf
+++ main.tf
@@ -37,6 +37,12 @@ resource "aws_security_group" "my-group" {
     }]
 }
 
+resource "aws_cloudfront_distribution" "s3_distribution" {
+    viewer_certificate {
+      cloudfront_default_certificate = true
+      minimum_protocol_version       = "TLSv1"
+}
+
 resource "aws_security_group" "my-group-ssh" {
     name        = "allow_tls"
     description = "Allow TLS inbound traffic"
